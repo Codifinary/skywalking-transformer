4c4cf6cfaf3910b1d7ef498aadf1c1f32d0730a4
Shibani <shiibs@Shibanis-MacBook-Air.local>
2025-08-22 23:30:35 +0530

    added return empty string for random id generator on error

--- converter/converter.go
+++ converter/converter.go
@@ -15,8 +15,8 @@ func randomTraceID() string {
 	b := make([]byte, 16)
 	if _, err := rand.Read(b); err != nil {
 		log.Printf("Failed to generate random trace ID: %v", err)
-		// Fallback to a default value or handle error appropriately
-		return "00000000000000000000000000000000"
+		// Fallback to a default value
+		return ""
 	}
 	return hex.EncodeToString(b)
 }
@@ -26,7 +26,7 @@ func randomSpanID() string {
 	if _, err := rand.Read(b); err != nil {
 		log.Printf("Failed to generate random span ID: %v", err)
 		// Fallback to a default value
-		return "0000000000000000"
+		return ""
 	}
 	return hex.EncodeToString(b)
 }
